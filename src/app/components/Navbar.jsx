9fc783e45c5e6ec21c362abf115860fd3457a244
rzkcp <syauqireza28@gmail.com>
2024-12-06 12:57:19 +0700

    tambah fitur project

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -11,6 +11,10 @@ const navLinks = [
     path: "#about",
   },
   {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
     title: "Contact",
     path: "#contact",
   },
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 
@@ -11,7 +12,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
-
+        <ProjectsSection />
         <EmailSection />
       </div>
       <Footer />
